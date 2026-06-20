ec27cf21b61ca81ff6205a0ddd6d963293c663b6
ashishchaubey <ashish.23b0121172@abes.ac.in>
2026-06-20 20:20:07 +0530

    initial commit

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,69 @@
+import Order from '../models/Order.js';
+import Cart from '../models/Cart.js';
+import Product from '../models/product.js';
+
+export const placeOrder = async (req, res) => {
+    try {
+        const { userId, address } = req.body;
+
+        // Get Cart
+        const cart = await Cart.findOne({ userId }).populate('items.productId');
+
+        if (!cart || cart.items.length === 0) {
+            return res.status(400).json({
+                message: "Cart is empty"
+            });
+        }
+
+        // Prepare Order Items
+        const orderItems = cart.items.map(item => ({
+            productId: item.productId._id,
+            quantity: item.quantity,
+            price: item.productId.price,
+        }));
+
+        // Calculate Total Amount
+        const totalAmount = orderItems.reduce(
+            (total, item) => total + (item.price * item.quantity),
+            0
+        );
+
+        // Deduct Product Stock
+        for (let item of cart.items) {
+            await Product.findByIdAndUpdate(
+                item.productId._id,
+                {
+                    $inc: { stock: -item.quantity }
+                }
+            );
+        }
+
+        // Create Order
+        const order = await Order.create({
+            userId,
+            items: orderItems,
+            address,
+            totalAmount,
+            paymentMethod: "COD",
+        });
+
+        // Clear Cart
+        await Cart.findOneAndUpdate(
+            { userId },
+            { items: [] }
+        );
+
+        // Success Response
+        res.status(201).json({
+            message: "Order placed successfully",
+            orderId: order._id
+        });
+
+    } catch (error) {
+        console.log(error);
+
+        res.status(500).json({
+            message: "Internal server error"
+        });
+    }
+};
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,45 +1,43 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import ProductDetails from "./pages/ProductDetails";
-
-import ProductList from "./admin/ProductList";
 import AddProduct from "./admin/AddProduct";
 import EditProduct from "./admin/EditProduct";
+import ProductList from "./admin/ProductList";
+import Navbar from "./components/Navbar";
+import Cart from "./pages/Cart";
+import CheckoutAddress from "./pages/CheckoutAddress";
+import Checkout from "./pages/Checkout";
+ import OrderSuccess from "./pages/OrderSuccess.jsx";
+
+function Layout() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+}
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/signup",
-    element: <Signup />,
-  },
-  {
-    path: "/product/:id",
-    element: <ProductDetails />,
-  },
-   { path: "/products", element: <ProductList /> },
+    element: <Layout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/login", element: <Login /> },
+      { path: "/signup", element: <Signup /> },
+      { path: "/product/:id", element: <ProductDetails /> },
+      { path : "/cart", element: <Cart />},
 
-  // Admin Routes
-  {
-    path: "/admin/products",
-    element: <ProductList />,
-  },
-  {
-    path: "/admin/products/add",
-    element: <AddProduct />,
-  },
-  {
-    path: "/admin/products/edit/:id",
-    element: <EditProduct />,
+      { path: "/admin/products", element: <ProductList /> },
+      { path: "/admin/products/add", element: <AddProduct /> },
+      { path: "/admin/products/edit/:id", element: <EditProduct /> },
+       { path: "/checkout-address", element: <CheckoutAddress /> },
+      { path: "/checkout", element: <Checkout /> },
+      { path: "/order-success/:id", element:<OrderSuccess /> }
+    ],
   },
 ]);
 
